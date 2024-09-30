bed234299a46ea26cdf99c3b164a67c1b1232dc3
Ari205553 <Ari205553@student-schoolsofwestfield.org>
2024-09-30 16:20:04 +0000

    guessGame

--- pseudocode/done.js
+++ pseudocode/done.js
@@ -27,3 +27,31 @@ var answer and store (1-100);
 // or if guess is greater than answer display "Too high!" 
 
 // or display "Too low!" 
+
+
+/* Simple Guess Program */
+
+// define function guessGame
+function guessGame(){
+  // generate a random integer 1-100
+var answer = Math.floor(Math.random()*100)+1;
+  // display the integer for testing purposes only
+  alert(answer);
+  // create guess and set equal to 0 
+var guess = 0;
+  // loop while guess is not equal to answer
+while (guess != answer){
+    // set guess equal to asking user to "Guess a number, 1-100"
+guess = prompt("Guess a number 1-100");
+    // if guess equals answer display "Correct!" 
+if ( guess == answer) alert("correct");
+    // or if guess is greater than answer display "Too high!" 
+else if (guess > answer) alert("Too HiGH");
+    // or if guess is less than answer display "Too low!" 
+else if (guess < answer) alert("too low");
+    // else say "Bad input"
+else alert("Bad input");
+  // end the loop
+}
+// end the function
+ }
--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 /* Simple Guess Program */
-// create variable called guess, set to 0
-var guess = ("0");
-// create variable called answer and store a random integer 1-100
-var answer and store (1-100);
-// display the answer for testing purposes only
 
-// set guess equal to prompt asking user to "Guess a number, 1-100"
-
-// if guess equals answer display "Correct!" 
-
-// or if guess is greater than answer display "Too high!" 
-
-// or display "Too low!" 
+// define function guessGame
+ function guessGame(){
+  // generate a random integer 1-100
+var answer = Math.floor(Math.random()*100)+1;
+  // display the integer for testing purposes only
+  alert(answer);
+  // create guess and set equal to 0 
+var guess = 0;
+  // loop while guess is not equal to answer
+while (guess != answer){
+    // set guess equal to asking user to "Guess a number, 1-100"
+guess = prompt("Guess a number 1-100");
+    // if guess equals answer display "Correct!" 
+if ( guess == answer) alert("correct");
+    // or if guess is greater than answer display "Too high!" 
+else if (guess > answer) alert("Too HiGH");
+    // or if guess is less than answer display "Too low!" 
+else if (guess < answer) alert("too low");
+    // else say "Bad input"
+else alert("Bad input");
+  // end the loop
+}
+// end the function
+ }
